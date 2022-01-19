0420e7a665d883f31654da849f8f76017f0a87a4
JoseIgnacioTapia <josignaciot@gmail.com>
2022-01-19 18:09:11 -0300

    Category Label Component

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -2,27 +2,21 @@ import fs from 'fs';
 import path from 'path';
 import Link from 'next/link';
 import matter from 'gray-matter';
-import Layout from '../components/Layout';
-import Post from '../components/Post';
+import Layout from '../../components/Layout';
+import Post from '../../components/Post';
 
-import { sortByDate } from '../utils/index.js';
+import { sortByDate } from '../../utils/index.js';
 
-export default function HomePage({ posts }) {
+export default function BlogPage({ posts }) {
   return (
     <Layout>
-      <h1 className="text-5xl border-b-4 p-5">Latest Posts</h1>
+      <h1 className="text-5xl border-b-4 p-5">Blog</h1>
 
       <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
         {posts.map((post, index) => (
           <Post key={index} post={post} />
         ))}
       </div>
-
-      <Link href="/blog">
-        <a className="block text-center border border-gray-500 text-gray-800 rounded-md py-4 my-5 transition duration-500 ease select-none hover:text-white hover:bg-gray-900 focus:outline-none focus:shadow-outline w-full">
-          All Posts
-        </a>
-      </Link>
     </Layout>
   );
 }
@@ -48,7 +42,7 @@ export async function getStaticProps() {
 
   return {
     props: {
-      posts: posts.sort((a, b) => sortByDate(a, b)).slice(0, 6),
+      posts: posts.sort((a, b) => sortByDate(a, b)),
     },
   };
 }
