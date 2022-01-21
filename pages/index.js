bb0b6078d09e2259dbf1d9d883e1c2c3c341a822
JoseIgnacioTapia <josignaciot@gmail.com>
2022-01-21 16:37:50 -0300

    Clean Up Get Posts

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import fs from 'fs';
-import path from 'path';
 import Link from 'next/link';
-import matter from 'gray-matter';
 import Layout from '@/components/Layout';
 import Post from '@/components/Post';
 
-import { sortByDate } from '@/utils/index.js';
+import { getPosts } from '@/lib/posts';
 
 export default function HomePage({ posts }) {
   return (
@@ -28,27 +25,9 @@ export default function HomePage({ posts }) {
 }
 
 export async function getStaticProps() {
-  const files = fs.readdirSync(path.join('posts'));
-
-  const posts = files.map(filename => {
-    const slug = filename.replace('.md', '');
-
-    const markdownWithMeta = fs.readFileSync(
-      path.join('posts', filename),
-      'utf-8'
-    );
-
-    const { data: frontmatter } = matter(markdownWithMeta);
-
-    return {
-      slug, // destructuring
-      frontmatter,
-    };
-  });
-
   return {
     props: {
-      posts: posts.sort((a, b) => sortByDate(a, b)).slice(0, 6),
+      posts: getPosts().slice(0, 6),
     },
   };
 }
